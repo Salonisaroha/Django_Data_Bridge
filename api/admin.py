021676034bac7365941219b2f3c0b506f419dba0
Saloni Saroha <salonisaroha5@gmail.com>
2024-10-13 17:01:30 +0530

    custom api

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from api.models import Company, Employee
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'type')
+    search_fields = ('name',)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('name', 'email','company')
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Employee)
+
+
